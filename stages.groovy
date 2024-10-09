f4edec819ae165cc8dd0586fc083dbb7ca66bca9
mytuo1 <44296607+mytuo1@users.noreply.github.com>
2024-10-09 16:21:44 +0300

    Update stages.groovy

--- stages.groovy
+++ stages.groovy
@@ -37,54 +37,13 @@ stage('Run Selenium Tests') {
 
 
 
-[13:08:59] E/launcher - UnsupportedOperationError: <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <link rel="stylesheet" type="text/css" href="/assets/displayhelpservlet.css" media="all"/>
-  <link href="/assets/favicon.ico" rel="icon" type="image/x-icon" />
-  <script src="/assets/jquery-3.1.1.min.js" type="text/javascript"></script>
-  <script src="/assets/displayhelpservlet.js" type="text/javascript"></script>
-  <script type="text/javascript">
-    var json = Object.freeze('{"consoleLink": "\u002fwd\u002fhub","type": "Standalone","class": "org.openqa.grid.web.servlet.DisplayHelpHandler$DisplayHelpServletConfig","version": "3.141.59"}');
-  </script>
-</head>
-<body>
-
-<div id="content">
-  <div id="help-heading">
-    <h1><span id="logo"></span></h1>
-    <h2>Selenium <span class="se-type"></span>&nbsp;v.<span class="se-version"></span></h2>
-  </div>
-
-  <div id="content-body">
-    <p>
-      Whoops! The URL specified routes to this help page.
-    </p>
-    <p>
-      For more information about Selenium <span class="se-type"></span> please see the
-      <a class="se-docs">docs</a> and/or visit the <a class="se-wiki">wiki</a>.
-      <span id="console-item">
-        Or perhaps you are looking for the Selenium <span class="se-type"></span> <a class="se-console">console</a>.
-      </span>
-    </p>
-    <p>
-      Happy Testing!
-    </p>
-  </div>
-
-  <div>
-    <footer id="help-footer">
-      Selenium is made possible through the efforts of our open source community, contributions from
-      these <a href="https://github.com/SeleniumHQ/selenium/blob/master/AUTHORS">people</a>, and our
-      <a href="http://www.seleniumhq.org/sponsors/">sponsors</a>.
-   </footer>
-  </div>
- </div>
-
-</body>
-</html>
-    at parseHttpResponse (/usr/lib/node_modules/protractor/node_modules/selenium-webdriver/lib/http.js:534:11)
+[13:18:30] E/launcher - SessionNotCreatedError: Expected browser binary location, but unable to find binary in default location, no 'moz:firefoxOptions.binary' capability provided, and no binary flag set on the command line
+Build info: version: '3.141.59', revision: 'e82be7d358', time: '2018-11-14T08:25:53'
+System info: host: '1579f914ecc4', ip: '172.17.0.2', os.name: 'Linux', os.arch: 'amd64', os.version: '5.15.153.1-microsoft-standard-WSL2', java.version: '17.0.2'
+Driver info: driver.version: unknown
+remote stacktrace:
+    at Object.checkLegacyResponse (/usr/lib/node_modules/protractor/node_modules/selenium-webdriver/lib/error.js:546:15)
+    at parseHttpResponse (/usr/lib/node_modules/protractor/node_modules/selenium-webdriver/lib/http.js:509:13)
     at /usr/lib/node_modules/protractor/node_modules/selenium-webdriver/lib/http.js:441:30
     at processTicksAndRejections (node:internal/process/task_queues:95:5)
 From: Task: WebDriver.createSession()
@@ -98,3 +57,4 @@ From: Task: WebDriver.createSession()
     at _fulfilled (/usr/lib/node_modules/protractor/node_modules/q/q.js:834:54)
     at /usr/lib/node_modules/protractor/node_modules/q/q.js:863:30
     at Promise.promise.promiseDispatch (/usr/lib/node_modules/protractor/node_modules/q/q.js:796:13)
+[13:18:30] E/launcher - Process exited with error code 199
